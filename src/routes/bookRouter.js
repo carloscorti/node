31279a4facdf4ea49c48fb388f0e4006aa417db3
Carlos Corti <carlosacorti@hotmail.com>
2020-04-16 18:12:27 +0200

    starting mysql conection
    set mysql conection and rpomisify mysql response

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -1,46 +1,102 @@
 const express = require('express');
+const mysql = require('mysql');
+const debug = require('debug')('app:bookRouter');
+
+
+const config = {
+  host: 'localhost',
+  database: 'pslibrary',
+  user: 'nodelibrary',
+  password: 'nodelibrary'
+};
+
+class Database {
+  constructor(conf) {
+    this.connection = mysql.createConnection(conf);
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.connection.connect((err) => {
+        if (err) {
+          debug(`error connecting: ${err.stack}`);
+          return reject(err);
+        }
+        debug(`connected as id ${this.connection.threadId}`);
+        return resolve();
+      });
+    });
+  }
+
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(rows);
+      });
+    });
+  }
+
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end((err) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  }
+}
+
+const db = new Database(config);
+db.connect()
+  .then(() => db.close())
+  .catch((err) => debug(`There was an error :( ${err}`));
+
 
 // lo defino como una funcion para configuraciones dinamicas pasando argumentos
 function router(nav) {
   const bookRouter = express.Router();
 
-  const books = [
-    {
-      title: 'War and Peace',
-      genre: 'Historical Fiction',
-      author: 'Lev Tolstoy',
-      read: false
-    },
-    {
-      title: 'Les Miserables',
-      genre: 'Historical Fiction',
-      author: 'Victor Hugo',
-      read: false
-    },
-    {
-      title: 'The time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: true
-    },
-    {
-      title: 'A junrey into the center of the earth',
-      genre: 'Science Fiction',
-      author: 'Jules Verne',
-      read: false
-    },
-    {
-      title: 'The dark world',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false
-    },
-    {
-      title: 'The wind in the Willows',
-      genre: 'Fantasy',
-      author: 'Kenneth Grahame',
-      read: false
-    }
+  const books = [{
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: true
+  },
+  {
+    title: 'A junrey into the center of the earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  },
+  {
+    title: 'The dark world',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false
+  },
+  {
+    title: 'The wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  }
   ];
 
   bookRouter.route('/')
@@ -58,7 +114,9 @@ function router(nav) {
 
   bookRouter.route('/:id')
     .get((req, res) => {
-      const { id } = req.params;
+      const {
+        id
+      } = req.params;
       res.render(
         'bookView', {
           nav,
