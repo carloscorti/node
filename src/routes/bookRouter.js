c6f2f3fbe614259f5c29f0677c41d29268b95abc
Carlos Corti <carlosacorti@hotmail.com>
2020-04-17 17:27:38 +0200

    using .all as middleware in routing
    in bookRouter.js
    .all in bookRouter.route('/:id') to execute the asyn query for a single book on every instance of this route

--- src/routes/bookRouter.js
+++ src/routes/bookRouter.js
@@ -29,25 +29,35 @@ function router(nav, config) {
 
   // routing para cada libro segun id
   bookRouter.route('/:id')
-    .get((req, res) => {
+    // uso .all como midleware,
+    // en cada llamada dentro de esta ruta se ejecuta la fucion de .all
+    // siempre tengo que terminar con next() para que continue el programa
+    // si no uso next() queda detenido al final de la funcion y no continua
+    .all((req, res, next) => {
       (async (conf) => {
         const { id } = req.params;
         const db = new Database(conf);
         try {
           const book = await db.query('SELECT * FROM `book` WHERE `id` = ? LIMIT 1', [`${id}`]);
-          res.render(
-            'bookView', {
-              nav,
-              book: book[0],
-              title: 'Library',
-            }
-          );
+          [req.book] = book; // idem a req.book = book[0];
         } catch (err) {
           debug(`There was an error :( ${err}`);
         } finally {
           await db.close();
+          next();
         }
       })(config);
+    })
+
+    .get((req, res) => {
+      res.render(
+        'bookView',
+        {
+          nav,
+          book: req.book,
+          title: 'Library',
+        }
+      );
     });
 
   return bookRouter;
